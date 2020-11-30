02da41e42c1cf08e7d35c0b3c0dbeb49b6a1011b
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2020-11-29 17:41:27 -0800

    Use CTest (mainly for timeout support)

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -3,16 +3,5 @@
 ###############################
 # Run all mbed-benchtest tests
 
-cd build/tests
-
-echo ">> Running cmsis-rtos-validator"
-cd arm-cmsis-rtos-validator
-./cmsis_rtos_validator
-
-echo ">> Running event queue tests"
-cd ../events
-./equeue_c_test.exe
-./equeue_cxx_test.exe
-./equeue_equeue_test.exe
-./equeue_queue_test.exe
-
+cd build
+make run_tests
